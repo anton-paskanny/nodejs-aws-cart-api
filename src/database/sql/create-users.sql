55b6ed3ea9a1c2fa4e8f1cd6d59d0d10d36399ec
Anton Paskanny <anton.paskanny@gmail.com>
2024-07-27 19:47:35 +0200

    feat: add users sql scripts

--- src/database/sql/create-users.sql
+++ src/database/sql/create-users.sql
@@ -1,6 +1,8 @@
-create table users (
-	id uuid primary key default uuid_generate_v4(),
-	name text not null,
-	email text,
-	password text not null
-)
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL,
+    email TEXT UNIQUE,
+    password TEXT NOT NULL
+);
--- src/database/sql/fill-users.sql
+++ src/database/sql/fill-users.sql
@@ -1,4 +1,5 @@
-insert into users (name, email, password ) values
-	('John', 'john_lennon@mail.com', 'beatles4orefer'),
+INSERT INTO users (name, email, password) 
+VALUES
+    ('John', 'john_lennon@mail.com', 'beatles4orefer'),
     ('Dave', 'dave_ghrol@mail.com', 'foo_fighters_123'),
-	('Kurt', 'kurt_cobain2@mail.com', 'nirvana_qwerty!');
+    ('Kurt', 'kurt_cobain2@mail.com', 'nirvana_qwerty!');
