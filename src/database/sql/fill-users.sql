b41ca33470d5cba9cd72d13ecc255d8e62610951
Anton Paskanny <anton.paskanny@gmail.com>
2024-07-27 17:08:08 +0200

    feat: add user entity and sql fill script

---
+++ src/database/sql/fill-users.sql
@@ -0,0 +1,4 @@
+insert into users (name, email, password ) values
+	('John', 'john_lennon@mail.com', 'beatles4orefer'),
+    ('Dave', 'dave_ghrol@mail.com', 'foo_fighters_123'),
+	('Kurt', 'kurt_cobain2@mail.com', 'nirvana_qwerty!');
