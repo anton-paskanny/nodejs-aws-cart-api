2209a6f4b7dbfc76dce3cf2c6b1762809c61cddf
Anton Paskanny <anton.paskanny@gmail.com>
2024-08-01 16:58:09 +0200

    feat: update main.ts file

--- src/main.ts
+++ src/main.ts
@@ -32,3 +32,27 @@ export const handler: Handler = async (
   server = server ?? (await bootstrap());
   return server(event, context, callback);
 };
+
+// Use for local testing only
+/*
+import { NestFactory } from '@nestjs/core';
+
+import helmet from 'helmet';
+
+import { AppModule } from './app.module';
+
+const port = process.env.PORT || 4000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: (req, callback) => callback(null, true),
+  });
+  app.use(helmet());
+
+  await app.listen(port);
+}
+bootstrap().then(() => {
+  console.log('App is running on %s port', port);
+});*/
